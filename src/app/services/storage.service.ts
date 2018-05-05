772b2b6e6e927366d3aaedd86df2351ed5103fbd
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-05 18:46:07 +0200

    Add storage service

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class StorageService {
+
+  constructor() { }
+
+  public get authToken() {
+    return localStorage.getItem('auth_token');
+  }
+
+  public set authToken(token: string) {
+    localStorage.setItem('auth_token', token);
+  }
+
+  public get userId() {
+    return localStorage.getItem('user_id');
+  }
+
+  public set userId(id: string) {
+    localStorage.setItem('user_id', id);
+  }
+
+  public get userName() {
+    return localStorage.getItem('user_name');
+  }
+
+  public set userName(name: string) {
+    localStorage.setItem('user_name', name);
+  }
+}
